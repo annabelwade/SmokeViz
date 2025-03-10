7ba4202fb129db3b4229189a26d7f7b2e25d72d5
annabelwade <annawade@uw.edu>
2025-03-09 18:54:20 -0700

    Add data augmentation

--- deep_learning/model.py
+++ deep_learning/model.py
@@ -54,7 +54,17 @@ if 'noG18' in hyperparams:
 with open(dict_fp, 'rb') as handle:
     data_dict = pickle.load(handle)
 
-data_transforms = transforms.Compose([transforms.ToTensor()])
+if hyperparams['augmentation'] == 'True':
+    data_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
+        transforms.RandomRotation(degrees=90),
+        ])
+else:
+    data_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        ])
 
 train_set = SmokeDataset(data_dict['train'], data_transforms)
 val_set = SmokeDataset(data_dict['val'], data_transforms)
